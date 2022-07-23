bd40acd7c17199c2be144af5745ddf135b159720
Matacrat <48725508+Matacrat@users.noreply.github.com>
2022-07-23 08:03:22 -0400

    Teleport to orgin Fix

--- compileacs.bat
+++ compileacs.bat
@@ -2,4 +2,4 @@ rem @echo off
 cd .\acc
 acc D:\GitHub\Checkpoints\src\acs_src\FLOPPY.acs D:\GitHub\Checkpoints\src\acs\FLOPPY.o
 acc D:\GitHub\Checkpoints\src\acs_src\SPPRESET.acs D:\GitHub\Checkpoints\src\acs\SPPRESET.o
-timeout /t 6 /nobreak >nul
+timeout /t 3 /nobreak >nul
