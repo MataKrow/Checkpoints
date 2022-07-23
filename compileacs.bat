56763e514d9cf61c51bfc2a59e1ebb0fbaa634da
Matacrat <48725508+Matacrat@users.noreply.github.com>
2022-07-23 10:34:25 -0400

    Empty Presets. (I should probably develop a dynamic system...)

--- compileacs.bat
+++ compileacs.bat
@@ -2,4 +2,4 @@ rem @echo off
 cd .\acc
 acc D:\GitHub\Checkpoints\src\acs_src\FLOPPY.acs D:\GitHub\Checkpoints\src\acs\FLOPPY.o
 acc D:\GitHub\Checkpoints\src\acs_src\SPPRESET.acs D:\GitHub\Checkpoints\src\acs\SPPRESET.o
-timeout /t 3 /nobreak >nul
+timeout /t 4 /nobreak >nul
--- test.bat
+++ test.bat
@@ -4,12 +4,11 @@ REM sets the location of gzdoom.
 
 
 :choice
-set /P c="0-GZDOOM 1-ZANDRONUM 3-GZDOOM Multiplayer test :"
+set /P c="0-GZDOOM 1-ZANDRONUM 2-GZDOOM Multiplayer test :"
 echo.
 if /I "%c%" EQU "0" goto :opt0
 if /I "%c%" EQU "1" goto :opt1
 if /I "%c%" EQU "2" goto :opt2
-if /I "%c%" EQU "3" goto :opt3
 goto :choice
 
 :opt0
@@ -81,12 +80,6 @@ exit
  echo Done replacing.
 move .\BUILD.ipk3 D:\Doom\SourcePorts\GZDoom
 cd D:\Doom\SourcePorts\GZDoom
-start gzdoom.exe -host 7 +name Player -noautoload  +vid_fullscreen 0 +developer 2 +idmypos 1 +vid_fps 1 +am_cheat 2 +noise 0 +map NULL +sv_cheats 1 -noidle +vid_defwidth 800 +vid_defheight 450 -0 +win_x 41 +win_y 523
-start gzdoom.exe -join DESKTOP-L61LU53 +name Player2 -noautoload +vid_fullscreen 0 +developer 2 +idmypos 1 +vid_fps 1 +map NULL +am_cheat 2 +noise 0 +sv_cheats 1 -noidle +vid_defwidth 800 +vid_defheight 450 +win_x 864 +win_y 523
-start gzdoom.exe -join DESKTOP-L61LU53 +name Player2 -noautoload +vid_fullscreen 0 +developer 2 +idmypos 1 +vid_fps 1 +map NULL +am_cheat 2 +noise 0 +sv_cheats 1 -noidle +vid_defwidth 800 +vid_defheight 450 +win_x 864 +win_y 523
-start gzdoom.exe -join DESKTOP-L61LU53 +name Player2 -noautoload +vid_fullscreen 0 +developer 2 +idmypos 1 +vid_fps 1 +map NULL +am_cheat 2 +noise 0 +sv_cheats 1 -noidle +vid_defwidth 800 +vid_defheight 450 +win_x 864 +win_y 523
-start gzdoom.exe -join DESKTOP-L61LU53 +name Player2 -noautoload +vid_fullscreen 0 +developer 2 +idmypos 1 +vid_fps 1 +map NULL +am_cheat 2 +noise 0 +sv_cheats 1 -noidle +vid_defwidth 800 +vid_defheight 450 +win_x 864 +win_y 523
-start gzdoom.exe -join DESKTOP-L61LU53 +name Player2 -noautoload +vid_fullscreen 0 +developer 2 +idmypos 1 +vid_fps 1 +map NULL +am_cheat 2 +noise 0 +sv_cheats 1 -noidle +vid_defwidth 800 +vid_defheight 450 +win_x 864 +win_y 523
-start gzdoom.exe -join DESKTOP-L61LU53 +name Player2 -noautoload +vid_fullscreen 0 +developer 2 +idmypos 1 +vid_fps 1 +map NULL +am_cheat 2 +noise 0 +sv_cheats 1 -noidle +vid_defwidth 800 +vid_defheight 450 +win_x 864 +win_y 523
-
+start .gzdoom.exe -host 2 +name Player -noautoload  +vid_fullscreen 0 +developer 2 +idmypos 1 +vid_fps 1 +am_cheat 2 +noise 0 +map NULL +sv_cheats 1 -noidle +vid_defwidth 800 +vid_defheight 450 -0 +win_x 41 +win_y 523
+start .gzdoom.exe -join DESKTOP-L61LU53 +name Player2 -noautoload +vid_fullscreen 0 +developer 2 +idmypos 1 +vid_fps 1 +map NULL +am_cheat 2 +noise 0 +sv_cheats 1 -noidle +vid_defwidth 800 +vid_defheight 450 +win_x 864 +win_y 523
 exit
