ab5df31940e1aa7dc9a0a94d30b1b5326697163f
Matacrat <48725508+Matacrat@users.noreply.github.com>
2022-07-27 11:31:15 -0400

    Fix? 7/27/22

--- compileacs.bat
+++ compileacs.bat
@@ -1,6 +1,5 @@
 @echo off
-rem ACC PATH
-cd D:\Doom\Tools\UltimateDoomBuilder\Compilers\Zandronum
+cd .\acc
 acc D:\GitHub\Checkpoints\src\acs_src\FLOPPY.acs D:\GitHub\Checkpoints\src\acs\FLOPPY.o
 acc D:\GitHub\Checkpoints\src\acs_src\SPPRESET.acs D:\GitHub\Checkpoints\src\acs\SPPRESET.o
 timeout /t 4 /nobreak >nul
